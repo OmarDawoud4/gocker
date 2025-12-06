7eb5a51440cba8ad0c5fe1ffdf118ebbfecc27e0
dawoud <UG_31093938@f-eng.tanta.edu.eg>
2025-12-06 18:29:17 +0200

    implement container namespace isolation( PID ,UTS ,Mount)

---
+++ README.md
@@ -0,0 +1,25 @@
+![Always has been](meme.png)
+
+# Wait, Docker is just Linux?
+
+> "Always has been"
+
+A minimal container runtime implementation in Go.
+Basically just a wrapper around Linux namespaces and chroot.
+
+## Usage
+
+1. Get a rootfs (alpine is good):
+```bash
+mkdir rootfs
+# download alpine tar and extract to rootfs/
+```
+
+2. Build and run:
+```bash
+go build
+sudo ./gocker run /bin/sh
+```
+
+## How it works
+It uses `CLONE_NEWPID` and `CLONE_NEWUTS` to isolate the process, and `chroot` to isolate the filesystem.
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("invalid args")
+	}
+
+	switch os.Args[1] {
+	case "run":
+		run()
+	case "child":
+		child()
+	default:
+		panic("unknown command")
+	}
+}
+
+func run() {
+	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
+
+	args := append([]string{"child"}, os.Args[2:]...)
+	cmd := exec.Command("/proc/self/exe", args...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as pid %d\n", os.Args[2:], os.Getpid())
+
+	must(syscall.Sethostname([]byte("container")))
+	must(syscall.Chroot("./rootfs"))
+	must(syscall.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+
+	must(syscall.Unmount("proc", 0))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
